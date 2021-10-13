71de5c5a0be7b481b411a4274d7567930d8ba761
taitamfc <taitam8590@gmail.com>
2021-10-13 10:16:42 +0700

    011-bt

---
+++ 011/bt/001.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+	[Bài tập] Đếm số lần xuất hiện của phần tử 
+	
+	$count 		= 0;
+	$value 		= 1;
+	$numbers 	= [1,2,1];
+
+	- DUyệt vòng lặp, mỗi lần $numbers[$i] == $value; $count++
+
+*/
---
+++ 011/bt/003.php
@@ -0,0 +1,40 @@
+<?php
+/* 
+	Xử lý post từ form
+	$string_numbers = '0911222333,0931222333,0971222333';
+	$numbers 		= explode(',',$string_numbers);	
+	
+	$vina 		= '091';
+	$mobi 		= '093';
+	$viettel_1 	= '097';
+	$viettel_2 	= '098';
+	
+	
+
+	$numbers = [
+		'0911222333',
+		'0941222333',
+		'0931222333',
+		'0971222333',
+		'0981222333',
+	];
+	
+
+	$number_vinas 		= [];
+	$number_mobis 		= [];
+	$number_viettels 	= [];
+	//Duyệt mảng
+		$number = '0911222333';//091
+
+		//https://www.php.net/manual/en/function.substr.php
+		$number 	= '0911222333';//091
+		$so_dau 	= substr($number,0,3);//091
+		$number_vinas[] = $number;
+
+
+
+
+	Duyệt mảng $numbers
+
+*/
+	
